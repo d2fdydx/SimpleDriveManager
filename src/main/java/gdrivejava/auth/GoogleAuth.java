2be61e665f73ce7be4ba2e8a3a0e24a5cc90ff3c
d2fdydx <devyat192002@gmail.com>
2015-06-18 21:07:53 +0800

    init

---
+++ src/main/java/gdrivejava/auth/GoogleAuth.java
@@ -0,0 +1,5 @@
+package gdrivejava.auth;
+
+public class GoogleAuth {
+
+}
---
+++ src/main/java/gdrivejava/main/DriveMain.java
@@ -0,0 +1,15 @@
+package gdrivejava.main;
+
+import gdrivejava.auth.GoogleAuth;
+
+public class DriveMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//init
+		GoogleAuth auth = new GoogleAuth();
+	}
+
+}
